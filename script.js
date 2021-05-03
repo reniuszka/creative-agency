7e173b196a8ebd0add11651e643bf094f633cd7f
Renat Diurczak <49594210+reniuszka@users.noreply.github.com>
2021-05-03 22:30:16 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-// creating variables to grab those classes fromm DOM Object Model to change or impact them
+
 
 const toggleButton = document.querySelector('.toggle');
 const navigation = document.querySelector('.navigation');
 
-//add event listener to lister to click event to take our toggleButton to add the class of active to toggle and navigation and remove it if it has, so we use toggle method
+
 
 toggleButton.addEventListener('click', () => {
   toggleButton.classList.toggle('active');
